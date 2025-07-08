1b5a671d932b5a0089a2fc3bb41564ff42f79492
cjsqudwns <cjsqudwns2781@gmail.com>
2025-07-08 17:30:05 +0900

    feat: JwtTokenProvider 추가 메소드 작성

--- src/main/java/com/bootcamp/savemypodo/config/JwtTokenProvider.java
+++ src/main/java/com/bootcamp/savemypodo/config/JwtTokenProvider.java
@@ -1,7 +1,6 @@
 package com.bootcamp.savemypodo.config;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
@@ -56,5 +55,28 @@ public class JwtTokenProvider {
                 .setExpiration(expiry)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException |
+                 ExpiredJwtException | UnsupportedJwtException |
+                 IllegalArgumentException e) {
+            return false;
+        }
     }
 }
