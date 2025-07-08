748cf7ae129094ea68d060df8d021508f4340bba
cjsqudwns <cjsqudwns2781@gmail.com>
2025-07-08 14:28:40 +0900

    init

---
+++ src/main/java/com/bootcamp/savemypodo/SaveMyPodoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.savemypodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveMyPodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveMyPodoApplication.class, args);
+    }
+
+}
