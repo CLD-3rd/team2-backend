61aa9a27f519c865edef8f4b5db21f1abb847100
cjsqudwns <cjsqudwns2781@gmail.com>
2025-07-10 16:25:46 +0900

    feat: Redis RefreshToken Service 작성

---
+++ src/main/java/com/bootcamp/savemypodo/service/RedisService.java
@@ -0,0 +1,29 @@
+package com.bootcamp.savemypodo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    // 저장
+    public void saveRefreshToken(String userId, String token, long duration, TimeUnit unit) {
+        redisTemplate.opsForValue().set("refreshToken:" + userId, token, duration, unit);
+    }
+
+    // 조회
+    public String getRefreshToken(String userId) {
+        return redisTemplate.opsForValue().get("refreshToken:" + userId);
+    }
+
+    // 삭제
+    public void deleteRefreshToken(String userId) {
+        redisTemplate.delete("refreshToken:" + userId);
+    }
+}
